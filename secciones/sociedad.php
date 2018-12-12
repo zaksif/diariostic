70f2a5d1effbac1f26d2750e356a38efe5d87fc3
Manuel Torres Gil <mtorres@ual.es>
2018-12-12 22:18:16 +0100

    Vuelta al origen

--- secciones/sociedad.php
+++ secciones/sociedad.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $sociedad = [
-"titulo" => "Hueglga en Cataluña",
+"titulo" => "XXX",
 "autor" => "Gabinete de comunicación",
-"resumen" => "Los médicos desconvocan la huelga tras aceptar la oferta del Govern de inyectar 100 millones en la primaria",
+"resumen" => "XXX",
 ];
 ?>
