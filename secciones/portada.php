f53e2dbc218a77a13fafc0a7e47732022d9560b2
Manuel Torres Gil <mtorres@ual.es>
2018-12-13 13:21:40 +0100

    vuelta al origen

--- secciones/portada.php
+++ secciones/portada.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "Portada",
-"autor" => "mtorres",
-"resumen" => "Bla, bla, bla",
+"titulo" => "XXX",
+"autor" => "XXX",
+"resumen" => "XXX",
 ];
 ?>
