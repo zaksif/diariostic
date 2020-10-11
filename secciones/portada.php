67c496cb933f4aff2ffe486bde07d9c8c6a69d8a
Manolo Torres <mtorres@ual.es>
2020-10-12 00:14:00 +0200

    Update portada.php

--- secciones/portada.php
+++ secciones/portada.php
@@ -4,7 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "POrtada maravillosa",
+"titulo" => "Portada maravillosa",
 "autor" => "mtorres",
 "resumen" => "Maravilla de portada",
 ];
