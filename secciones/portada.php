1343eee0c8bb846140c6317972ae590c6d510408
Manolo Torres <mtorres@ual.es>
2020-10-27 17:49:57 +0100

    Portada sublime

--- secciones/portada.php
+++ secciones/portada.php
@@ -4,7 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "Portada maravillosa",
+"titulo" => "Portada maravillosa y sublime",
 "autor" => "mtorres",
 "resumen" => "Maravilla de portada",
 ];
