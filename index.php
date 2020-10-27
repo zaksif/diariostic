ddc2c0f0f24e914a89a663304759f450e030eeb5
Manolo Torres <mtorres@ual.es>
2020-10-27 17:32:22 +0100

    Actualizar portada

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
       <header class="blog-header py-3">
         <div class="row flex-nowrap justify-content-between align-items-center">
           <div class="col-12 text-center">
-            <a class="blog-header-logo text-dark" href="#">Diario STIC</a>
+            <a class="blog-header-logo text-dark" href="#">Diario STIC actualizado</a>
           </div>
         </div>
       </header>
