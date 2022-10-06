de4c35a499bd230f525cf04b0e767eb3168689f5
Manolo Torres <mtorres@ual.es>
2022-10-06 18:02:27 +0200

    Update portada.php

--- secciones/portada.php
+++ secciones/portada.php
@@ -4,7 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "La portada HUT",
+"titulo" => "La portada CC2022",
 "autor" => "mtorres",
 "resumen" => "El resumen maravilloso",
 ];
